d71ad86ef35d27358349a53475497fd56823bb2e
Juliherms Vasconcelos <j.a.vasconcelos321@gmail.com>
2024-09-24 14:50:56 -0300

    feat: add roboto fonts

--- App.tsx
+++ App.tsx
@@ -1,11 +1,17 @@
 import { Groups } from "@/screens/groups"
+import { ActivityIndicator } from "react-native";
 import { ThemeProvider } from "styled-components";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import  theme from './src/theme';
 
 export default function App() {
+
+  //carrega as fontes - o carregamento de fontes eh assincono
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      { fontsLoaded ? <Groups /> : <ActivityIndicator /> }
     </ThemeProvider>    
   );
 }
---
+++ README.md
@@ -0,0 +1,18 @@
+
+
+### Requisitos
+
+- Node v18.12.1
+- NPM 8.19.2
+
+### Para executar o frontend
+```bash
+$ nom install
+```
+```bash
+$ npx expo start
+```
+
+
+
+
