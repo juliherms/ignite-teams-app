ee5be4047b3e89a280eb03154107dc8698de3788
Juliherms Vasconcelos <j.a.vasconcelos321@gmail.com>
2024-09-24 23:01:17 -0300

    feat: add status bar

--- src/screens/groups/styles.ts
+++ src/screens/groups/styles.ts
@@ -3,6 +3,5 @@ import styled from "styled-components/native";
 export const Container = styled.View`
     flex: 1;
     background-Color: ${({ theme }) => theme.COLORS.GRAY_500};
-    align-items: center;
-    justify-content: center;
+    padding: 24px;
 `;
