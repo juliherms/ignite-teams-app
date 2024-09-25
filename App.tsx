d219cd183cd89917e6de3cc748c0c5d41eb923ff
Juliherms Vasconcelos <j.a.vasconcelos321@gmail.com>
2024-09-24 22:30:24 -0300

    feat: add status bar

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { Loading } from "@/components/Loading";
 import { ThemeProvider } from "styled-components";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
 import  theme from './src/theme';
+import { StatusBar } from "react-native"
 
 export default function App() {
 
@@ -11,6 +12,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar 
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent={true}
+      />
       { fontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>    
   );
